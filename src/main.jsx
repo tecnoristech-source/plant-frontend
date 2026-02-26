dbcc91e01585ed3a3d30e113242c2fd5dc043a1e
tecnoristech-source <tecnoris.tech@gmail.com>
2026-02-26 22:16:05 +0530

    Refactor rendering to include React.StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
-
-import { ThemeProvider } from "./context/ThemeContext";
-
-<BrowserRouter>
-  <ThemeProvider>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </ThemeProvider>
-</BrowserRouter>
